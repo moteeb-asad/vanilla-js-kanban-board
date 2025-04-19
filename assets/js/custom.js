79fd4a7632eeef74171270ed9b5fdb8ebdf8fdb5
moteeb <moteeb83@gmail.com>
2025-04-19 11:16:39 +0500

    first commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,19 @@
+document.addEventListener('DOMContentLoaded',function(){
 
-function toDo(){
-    console.log("Testing");
-}
+    let draggables = document.querySelectorAll('.draggable');
+
+    draggables.forEach(draggable => () => {
+        console.log("Testing", draggable);
+    })
+
+
+});
+
+
+// let taskCard = document.getElementsByClassName('task-card');
+
+
+// function toDo(){
+//     console.log("Testing");
+
+// }
